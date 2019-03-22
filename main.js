f83a0a9184484b6856be5bd659cbca075b3db641
Luis E. Sánchez P <42817881+lesanpi@users.noreply.github.com>
2019-03-22 14:15:10 -0400

    arreglando detalles responsive
    Arreglando detalles del responsive, agregamos pagina de post, cambio de color de header al scroll y arreglo del grid del header

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ $header = document.getElementById('header');
 $tw = document.getElementById('twitter');
 $ig = document.getElementById('ig');
 $fb = document.getElementById('facebook');
-console.log('hola')
+$left = document.getElementById('left');
+
+console.log(window.innerWidth)
 
 window.onscroll = function() {
 	if(window.scrollY > 350){
-		
-		$header.style.background = 'white'
+				
+		$header.style.background = '#3eb6d1'
 		$header.style.boxShadow = '1px 1px 1px 1px rgba(70, 70, 70, .4)'
-		$tw.style.fill = '#3eb6d1'
+		/*$tw.style.fill = '#3eb6d1'
 		$ig.style.fill = '#3eb6d1'
-		$fb.style.fill = '#3eb6d1'
+		$fb.style.fill = '#3eb6d1'*/
 		
 	}
 	if(window.scrollY <= 350){
@@ -22,4 +24,15 @@ window.onscroll = function() {
 		$ig.style.fill = 'white'
 		$fb.style.fill = 'white'
 	}
+
+	if (window.scrollY >= 450 && window.innerWidth > 770) {
+
+		$left.style.position = 'fixed'
+		$left.style.top = '100px'
+		console.log('left')
+	}
+	if(window.scrollY < 450 && window.innerWidth > 770 ){
+		$left.style.position = 'initial'
+	}
+	
 }
---
+++ post.js
@@ -0,0 +1,38 @@
+$header = document.getElementById('header');
+$tw = document.getElementById('twitter');
+$ig = document.getElementById('ig');
+$fb = document.getElementById('facebook');
+$left = document.getElementById('left');
+
+console.log(window.innerWidth)
+
+window.onscroll = function() {
+	if(window.scrollY > 350){
+				
+		$header.style.background = '#3eb6d1'
+		$header.style.boxShadow = '1px 1px 1px 1px rgba(70, 70, 70, .4)'
+		/*$tw.style.fill = '#3eb6d1'
+		$ig.style.fill = '#3eb6d1'
+		$fb.style.fill = '#3eb6d1'*/
+		
+	}
+	if(window.scrollY <= 350){
+		
+		$header.style.background = 'transparent'
+		$header.style.boxShadow = 'none'
+		$tw.style.fill = 'white'
+		$ig.style.fill = 'white'
+		$fb.style.fill = 'white'
+	}
+
+	if (window.scrollY >= 450 && window.innerWidth > 890) {
+
+		$left.style.position = 'fixed'
+		$left.style.top = '100px'
+		console.log('left')
+	}
+	if(window.scrollY < 450 && window.innerWidth > 890 ){
+		$left.style.position = 'initial'
+	}
+	
+}
