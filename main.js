9557d08b682f37efb05daac8cc87a7a5ea2dbb7f
Luis E. Sánchez P <42817881+lesanpi@users.noreply.github.com>
2019-03-23 19:30:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,17 +3,42 @@ $tw = document.getElementById('twitter');
 $ig = document.getElementById('ig');
 $fb = document.getElementById('facebook');
 $left = document.getElementById('left');
+$logo = document.getElementById('logo');
+const $shares = document.getElementsByClassName('share');
+const $sharesList = document.getElementsByClassName('share-list');
 
+console.log($shares)
 console.log(window.innerWidth)
+
+
+$shares[0].addEventListener('click', (event) => {
+    $sharesList[0].classList.toggle('visible')
+});
+$shares[1].addEventListener('click', (event) => {
+    $sharesList[1].classList.toggle('visible')
+});
+$shares[2].addEventListener('click', (event) => {
+    $sharesList[2].classList.toggle('visible')
+});
+/*$shares[3].addEventListener('click', (event) => {
+    $sharesList[3].classList.toggle('visible')
+});
+$shares[4].addEventListener('click', (event) => {
+    $sharesList[4].classList.toggle('visible')
+});
+$shares[5].addEventListener('click', (event) => {
+    $sharesList[5].classList.toggle('visible')
+});*/
 
 window.onscroll = function() {
 	if(window.scrollY > 350){
 				
-		$header.style.background = '#3eb6d1'
-		$header.style.boxShadow = '1px 1px 1px 1px rgba(70, 70, 70, .4)'
-		/*$tw.style.fill = '#3eb6d1'
+		$header.style.background = 'white'
+		$header.style.boxShadow = '1px 1px 1px 1px rgba(150, 150, 150, .4)'
+		$tw.style.fill = '#3eb6d1'
 		$ig.style.fill = '#3eb6d1'
-		$fb.style.fill = '#3eb6d1'*/
+		$fb.style.fill = '#3eb6d1'
+		$logo.style.background = '#3eb6d1'
 		
 	}
 	if(window.scrollY <= 350){
@@ -26,12 +51,13 @@ window.onscroll = function() {
 	}
 
 	if (window.scrollY >= 450 && window.innerWidth > 770) {
-
+		
 		$left.style.position = 'fixed'
 		$left.style.top = '100px'
-		console.log('left')
+		
 	}
 	if(window.scrollY < 450 && window.innerWidth > 770 ){
+		
 		$left.style.position = 'initial'
 	}
 	
